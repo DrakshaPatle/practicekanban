61b5588e02de6e71e9a1dbd21989bf1d5b32a7ef
DrakshaPatle <draksha@appsdeployer.com>
2023-03-10 16:25:24 +0530

    calender

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Layout from './components/Layout/Layout';
+import BoardPage from './pages/Board/Board';
+import Calendar from './pages/Calendar/Calendar';
+import Dashboard from './pages/Dashboard/Dashboard';
+import DataGrid from './pages/DataGrid/DataGrid';
 
-function App() {
-  return 
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Layout/>}>
 
-      <Route path="dashboard" element={<Dashboard/>}/>
-      
-    </Route>
+const App = () => {
+  return <div id="dashboard">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="dashboard" element={<Dashboard/>}/>
+          <Route path="calendar" element={<Calendar/>}/>
+          <Route path="board" element={<BoardPage/>}/>
+          <Route path="users" element={<DataGrid/>}/>
+        
+        </Route>
 
-  </Routes>
-</BrowserRouter>
-}
+      </Routes>
+    </BrowserRouter>
+  </div>
+};
 
 export default App;
